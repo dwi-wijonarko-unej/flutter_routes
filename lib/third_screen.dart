10c00d51b302ef6ea6e0fe0389e8ca42ecd69431
dwi wijonarko <dwi.wijonarko@gmail.com>
2025-04-22 07:39:11 +0700

    Add second and third screens with navigation functionality

--- lib/third_screen.dart
+++ lib/third_screen.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_routes/third_screen.dart';
+import 'package:flutter_routes/first_screen.dart';
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Layar Kedua'),
+        title: Text('Layar Ketiga'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Ini adalah layar kedua!!'),
+            Text('Ini adalah layar ketiga!!'),
             SizedBox(height: 20),
             ElevatedButton(
-              child: Text('Kembali ke Layar Pertama'),
+              child: Text('Kembali ke Layar Berapa?'),
               onPressed: () {
                 // Kembali ke layar sebelumnya dengan pop()
                 Navigator.pop(context);
@@ -25,13 +25,14 @@ class SecondScreen extends StatelessWidget {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              child: Text('Buka Layar Ketiga'),
+              child: Text('Kembali ke Layar Pertama'),
               onPressed: () {
-                // Navigasi ke ThirdScreen dengan push()
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ThirdScreen()),
-                );
+                // Kembali ke layar pertama dengan popUntil()
+                Navigator.popUntil(context, ModalRoute.withName('/'));
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => FirstScreen()),
+                // );
               },
             ),
           ],
